bb0749fc1da9edaa9532231974a7a4b56f573ba0
Nichoals Pipitone <npip99@gmail.com>
2020-08-21 19:09:31 -0400

    fully comment the entire compiler

--- voxelscript/src/utils.ts
+++ voxelscript/src/utils.ts
@@ -3,74 +3,89 @@ import * as path from "path";
 import { writeFileSync, readFileSync, readdirSync, statSync, existsSync, mkdirSync } from 'fs';
 import minimist from 'minimist';
 
-interface folder_information {
-    filename:string,
-    name:string,
-    data:string
+interface file_information {
+  filepath:string,
+  name:string,
+  data:string
 };
 interface vspackage {
-    package_name:string,
-    package_path:string,
-    options: any
-  };
-  interface option_type {
-    build_target : string,
-    source : string,
-    desired_module : string | null,
-    override : string | null
-  };
+  package_name:string,
+  package_path:string,
+  options: any
+};
+interface option_type {
+  build_target : string | null,
+  source : string,
+  desired_module : string | null,
+  override : string | null
+};
 
+// Check if child directory string is a descendent of the base directory string
 function is_subdir(base:string, child:string):boolean {
     let relative = path.relative(base, child);
     const isSubdir = (relative != "") && !relative.startsWith('..') && !path.isAbsolute(relative);
     return isSubdir;
 }
 
-let getAllSubfiles = (baseFolder : string, folderList : folder_information[] = []) => {
+// Get all .vs files in any subdirectories of a base folder
+let getAllVoxelScriptSubfiles = (baseFolder : string, fileList : file_information[] = []) => {
+  // Get folders and files
   let folders:string[] = readdirSync(baseFolder).filter(file => statSync(path.join(baseFolder, file)).isDirectory());
   let files:string[] = readdirSync(baseFolder).filter(file => !statSync(path.join(baseFolder, file)).isDirectory());
+
+  // Loop through all files
   for (let file of files) {
-    let filename = path.join(baseFolder, file);
-    if (path.basename(filename).match(/^\w+\.vs$/g)) {
-      let file_data = readFileSync(filename, 'utf8');
-      folderList.push({
-        filename: filename,
-        name: path.basename(filename).split('.')[0],
+    // Recreate filepath of file
+    let file_path = path.join(baseFolder, file);
+
+    // Only add if it's a \w+.vs file
+    if (path.basename(file_path).match(/^\w+\.vs$/g)) {
+      // Read the file and push to the folder list
+      let file_data = readFileSync(file_path, 'utf8');
+      fileList.push({
+        filepath: file_path,
+        name: path.basename(file_path).split('.')[0],
         data: file_data,
       });
     }
   }
+
+  // Recurse into all subfolders, accumulating files in fileList
   folders.forEach(folder => {
-    getAllSubfiles(path.join(baseFolder,folder), folderList);
+    getAllVoxelScriptSubfiles(path.join(baseFolder,folder), fileList);
   });
-  return folderList;
+
+  // Return the file list
+  return fileList;
 }
 
+// Find a vspackage.json in any parent directory, specifically return the first one that is found
 function get_package_json(pathname : string) : vspackage | null {
-    let dir = pathname;
-    while(true) {
-      let try_package_path = path.join(dir, 'vspackage.json');
-      let package_json_data;
-      try {
-        package_json_data = readFileSync(try_package_path, 'utf8');
-      } catch (err) {
-        // Here you get the error when the file was not found,
-        // but you also get any other error
-        if (err.code === 'ENOENT') {
-        } else {
-          console.log(try_package_path + ' not found! Error Code: ' + err.code);
-        }
+  // Start with the given pathname
+  let dir = pathname;
+
+  while(true) {
+    // Read vspackage.json if it exists
+    let try_package_path = path.join(dir, 'vspackage.json');
+    let package_json_data = null;
+    try {
+      package_json_data = readFileSync(try_package_path, 'utf8');
+    } catch (err) {
+      // If we find an expected error, we should log it
+      if (err.code != 'ENOENT') {
+        console.log(try_package_path + ' not found! Error Code: ' + err.code);
       }
-  
-      if (typeof(package_json_data) == 'string') {
-        return {
-          package_name: JSON.parse(package_json_data).name,
-          package_path: dir,
-          options: JSON.parse(package_json_data)
-        };
-      }
-  
-      // Move up to parent directory
+    }
+
+    // If package_json_data exists, return it
+    if (package_json_data) {
+      return {
+        package_name: JSON.parse(package_json_data).name,
+        package_path: dir,
+        options: JSON.parse(package_json_data)
+      };
+    } else {
+      // Otherwise, Move up to the parent directory
       let new_dir = path.dirname(dir);
       if (new_dir == dir) {
         return null;
@@ -78,153 +93,233 @@ function get_package_json(pathname : string) : vspackage | null {
       dir = new_dir;
     }
   }
+}
 
+// Print error messages. Example Error message is below:
+//
+// ~~~ Error parsing module Entity ~
+// >>> /home/npip99/programming/voxelcraft/Entity.vs:6:24 -> 6:25
+// 99  |     int entity_id();
+// 100 |
+// 101 |     bool exists() {
+// 102 |         return this.en tity_id() != 0;
+// 102 |                        ^
+// 103 |     }
+// 104 |
+// 105 |     int get_entity_id() {
+//     \--- Expected ".", "[","(", postfix operator, binary operator, "?", or ";", but character "t" found instead.
+//
+function error_to_string(module_name : string, file_path : string, code : string, err : any) {
+  // Padding of lines/columns around the ^'ed error message
+  
+  // How many lines above and below the ^ to print
+  const lines_padding = 3;
+  // How many columns left and right of the ^ to print
+  const column_padding = 40;
+  
+  // (Temp variables to hold line & column data, see usage below)
+  let start_data;
+  let end_data;
 
-  function error_to_string(module_name : string, file_path : string, code : string, err : any) {
-    const width = 80;
-    const height = 6;
-    
-    let start_data;
-    let end_data;
-    let msg;
-  
-    if (err.missing_dependency) {
-      start_data = err.missing_dependency.location.start;
-      end_data = err.missing_dependency.location.end;
-      msg = 'Missing Dependency: ' + err.missing_dependency.module_name;
-    } else {
-      start_data = err.location.start;
-      end_data = err.location.end;
-      msg = err.message;
+  // If there's a missing dependency, then we generate the error message accordingly
+  if (err.missing_dependency) {
+    start_data = err.missing_dependency.location.start;
+    end_data = err.missing_dependency.location.end;
+  } else {
+  // Otherwise, we just grab the standard one
+    start_data = err.location.start;
+    end_data = err.location.end;
+  }
+
+  // Get the lines of code in an array
+  let lines = code.split('\n');
+
+  // Get the start/end line and column for error messages
+  // This will show where to underline in red
+  let error_top_line = start_data.line;
+  let error_left_col = start_data.column;
+  let error_bottom_line = end_data.line;
+  let error_right_col = end_data.column;
+
+  // Make a window around the error location
+
+  // Calculate the leftmost column and rightmost column to display in the error message window
+  let left_col = error_left_col - column_padding;
+  let right_col = error_right_col + column_padding;
+  if (left_col < 1) left_col = 1;
+
+  // Calculate the topmost line and bottommost line to display in the error message window
+  let top_line = error_top_line - lines_padding;
+  if (top_line < 1) top_line = 1;
+  let bottom_line = error_bottom_line + lines_padding;
+  if (bottom_line > lines.length) bottom_line = lines.length;
+
+  // Calculate digits of the longest (largest) line number,
+  // as that will be the minimum padding of the rest of the lines
+  let max_num_digits = ("" + bottom_line).length;
+
+  // Begin accumulating output
+  let output = '';
+
+  // Print the name of the module that had an error, with a tilde band around it
+  // ~~~ Error parsing module Entity ~~~
+  // ... | ...
+  // 104 | ...
+  // 105 | ...
+  output += '\n';
+  let tilde_band = "";
+  for(let i = 0; i < max_num_digits; i++) {
+    tilde_band += "~";
+  }
+  output += tilde_band + ' Error parsing module ' + module_name + ' ' + tilde_band;
+  output += '\n';
+
+  // Print error filename and row & column error locations
+  // >>> /home/npip99/programming/voxelcraft/Entity.vs:6:24 -> 6:25
+  // ...
+  // 105 | ...
+  for(let i = 0; i < max_num_digits; i++) {
+    output += ">";
+  }
+  output += " " + file_path + ":" + error_top_line + ":" + error_left_col + ' -> ' + error_bottom_line + ':' + error_right_col + "\n";
+
+  // Create function to go from 1-indexed line and columns, to 0-indexed javascript arrays
+  let zero_index = (i : number) => i-1;
+
+  // Print each line individually
+  // 101 |     bool exists() {
+  for(let i = top_line; i <= bottom_line; i++) {
+    // Grab the line itself
+    let line = lines[zero_index(i)];
+
+    // Print line #
+    output += i;
+    // Pad the line based on number of missing digits versus number of digits on the last line
+    for(let j = 0; j < max_num_digits - ("" + i).length; j++) {
+      output += " ";
     }
-  
-    let lines = code.split('\n');
-  
-    let start_line = start_data.line;
-    let start_col = start_data.column;
-    let end_line = end_data.line;
-    let end_col = end_data.column;
-  
-    let left_col = start_col - width / 2;
-    let right_col = end_col + width / 2;
-    if (left_col < 1) left_col = 1;
-  
-    let top_line = start_line - height / 2;
-    if (top_line < 1) top_line = 1;
-    let bottom_line = end_line + height / 2;
-    if (bottom_line > lines.length) bottom_line = lines.length;
-  
-    let max_num_digits = ("" + bottom_line).length;
-  
-    let output = '';
-    
-    // Print error module
-    let band = "";
-    for(let i = 0; i < max_num_digits; i++) {
-      band += "~";
+
+    // Print a pipe to separate line # and the line itself
+    output += " | ";
+
+    // Print the line itself, from left column to right column
+    for(let column_number = left_col; column_number <= right_col && zero_index(column_number) < line.length; column_number++) {
+      // Get character
+      let character = line[zero_index(column_number)];
+
+      // Print character, while hiding \r
+      if (character == '\r') {
+        character = ' ';
+      }
+      output += character;
     }
-  
     output += '\n';
-    output += band + ' Error parsing module ' + module_name + ' ' + band + '\n';
-  
-    // Print error filename and row/col
-    for(let i = 0; i < max_num_digits; i++) {
-      output += ">";
-    }
-    output += " " + file_path + ":" + start_line + ":" + start_col + ' -> ' + end_line + ':' + end_col + "\n";
-  
-    for(let i = top_line; i <= bottom_line; i++) {
-      let line = lines[i-1];
-      // Print line #
+    
+    // Print ^ below any error regions of the line. E.g.
+    //
+    // 102 |         return this.en tity_id() != 0;
+    // 102 |                        ^
+    //
+    // If i is a line where the error has occured,
+    if (error_top_line <= i && i <= error_bottom_line) {
+      // Print line # again, and again pad any missing digits
       output += i;
       for(let j = 0; j < max_num_digits - ("" + i).length; j++) {
         output += " ";
       }
-      // Print line
+
+      // Print a line of spaces, but with ^ under any of the errored columns from the line above it
       output += " | ";
-      for(let c = left_col; c <= right_col && c - 1 < line.length; c++) {
-        let ch = line[c-1];
-        if (ch == '\r') {
-          ch = ' ';
+      for(let column_number = left_col; column_number <= right_col; column_number++) {
+        if (zero_index(column_number) >= line.length) {
+          break;
         }
-        output += ch;
-      }
-      output += '\n';
-      // Print ^ below error regions of the line
-      if (start_line <= i && i <= end_line) {
-        // Print line #
-        output += i;
-        for(let j = 0; j < max_num_digits - ("" + i).length; j++) {
-          output += " ";
-        }
-        // Print line with ^ under the marked columns
-        output += " | ";
-        for(let c = left_col; c <= right_col && c < line.length; c++) {
-          let should_point = true;
-          if (c < start_col && i == start_line) {
-            should_point = false;
-          }
-          // Clip off the end_col itself
-          if (end_col <= c && i == end_line) {
-            should_point = false;
-          }
-          if (should_point) {
-            output += "^";
-          } else {
-            output += " ";
-          }
-        }
-        output += "\n";
-      }
-    }
-    for(let j = 0; j < max_num_digits; j++) {
-      output += " ";
-    }
-    output += " \\--- ";
-    if (err.missing_dependency) {
-      output += msg + " (" + err.message + ")";
-      output += "\n";
-    } else {
-      output += "Expected ";
-  
-      // Format list of expected symbols
-      let expected_arr : string[] = [];
-      for(let e of err.expected) {
-        if (e.type == 'end') {
-          expected_arr.push("end of file");
-        } else if (e.text) {
-          expected_arr.push("\"" + e.text + "\"");
+
+        // Print a carrot if the column is an error column of that line
+        if (i == error_top_line && i == error_bottom_line) {
+          // If this is the only error line, the carrots print between the left and right error column
+          output += error_left_col <= column_number && column_number <= error_right_col ? "^" : " ";
+        } else if (i == error_top_line) {
+          // If it's only the top line, print after the left column (inclusive)
+          output += error_left_col <= column_number ? "^" : " ";
+        } else if (i == error_bottom_line) {
+          // If it's only the bottom line, print before the right column (inclusive)
+          output += column_number <= error_right_col ? "^" : " ";
         } else {
-          expected_arr.push(e.description);
+          // Always print a carrot if it's on the interior lines of a multi-line error
+          output += "^";
         }
       }
-      expected_arr = [...Array.from(new Set(expected_arr))];
-      if (expected_arr.length > 1) {
-        expected_arr[expected_arr.length - 1] = "or " + expected_arr[expected_arr.length - 1];
-      }
-      output += expected_arr.join(", ") + ", but ";
-      if (err.found) {
-        output += "character \"" + err.found + "\" found instead.\n";
-      } else {
-        output += "end of file was reached.\n";
-      }
+      output += "\n";
     }
-  
-    return output;
   }
 
+  // Print line that contains the error message
+  // ...
+  // 105 | 
+  // 106 |     int get_entity_id() {
+  //     \--- Expected ".", "[","(", postfix operator, binary operator, "?", or ";", but character "t" found instead.
+  //
+  for(let j = 0; j < max_num_digits; j++) {
+    output += " ";
+  }
+  output += " \\--- ";
+  if (err.missing_dependency) {
+    // Print the missing dependency error message
+    output += 'Missing Dependency: ' + err.missing_dependency.module_name + " (" + err.message + ")";;
+    output += "\n";
+  } else {
+    output += "Expected ";
+
+    // Create list of expected symbols
+    let expected_arr : string[] = [];
+    for(let e of err.expected) {
+      if (e.type == 'end') {
+        expected_arr.push("end of file");
+      } else if (e.text) {
+        expected_arr.push("\"" + e.text + "\"");
+      } else {
+        expected_arr.push(e.description);
+      }
+    }
+    
+    // Remove duplicates
+    expected_arr = [...Array.from(new Set(expected_arr))];
+
+    // Put the "or" in "W, X, Y, or Z"
+    if (expected_arr.length > 1) {
+      expected_arr[expected_arr.length - 1] = "or " + expected_arr[expected_arr.length - 1];
+    }
+
+    // Generate rest of the error message
+    // ".", "[","(", postfix operator, binary operator, "?", or ";", but character "t" was found instead.
+    // ~~~~~
+    // ".", "[","(", postfix operator, binary operator, "?", or ";", but end of file was found instead.
+    output += expected_arr.join(", ") + ", but ";
+    if (err.found) {
+      output += "character \"" + err.found + "\" was found instead.\n";
+    } else {
+      output += "end of file was found instead.\n";
+    }
+  }
+
+  // Return generated error message
+  return output;
+}
+
+// Use minimist to parse arguments
 function parse_args(args : any[]) : option_type {
   let argv = minimist(args.slice(2));
 
-  let build_target : string = "";
+  let build_target : string | null = "";
   let source : string = "";
   let desired_module : string | null = null;
   let override : string | null = null;
 
+  // Use path.resolve to get the absolute directories of each argument
+
   if (argv['build-target']) {
     build_target = path.resolve(argv['build-target']);
-  } else {
-    throw new Error("No --build-target given!");
   }
 
   if (argv['source']) {
@@ -249,4 +344,4 @@ function parse_args(args : any[]) : option_type {
   };
 }
 
-export {is_subdir, getAllSubfiles, get_package_json, error_to_string, parse_args};
+export {is_subdir, getAllVoxelScriptSubfiles, get_package_json, error_to_string, parse_args};
--- voxelscript/src/voxelscript_compiler.ts
+++ voxelscript/src/voxelscript_compiler.ts
@@ -2,56 +2,82 @@ import * as peg from 'pegjs';
 import { writeFileSync, readFileSync, readdirSync, statSync, existsSync, mkdirSync } from 'fs';
 import * as path from "path";
 import { VSCompilerContext } from './voxelscript_compiler_context';
-import { is_subdir, getAllSubfiles, get_package_json, error_to_string, parse_args } from './utils';
+import { is_subdir, getAllVoxelScriptSubfiles, get_package_json, error_to_string, parse_args } from './utils';
 const Tracer = require('pegjs-backtrace');
 
 const TRACE_PARSER = false;
 
 // Read and parse args
-
+// source => file directory to read files from
+// build_target => directory to put compiled files
+// desired_module => the module to compile, if a specific one is desired (compile all if null)
+// override => A set of files to override existing files in the directory
+//             (I.e., If Entity.vs is in source, and Entity.vs is in override, then use the one in override)
 let options = parse_args(process.argv);
 
-let pkg = get_package_json(options.source);
-if (pkg == null) {
+// Get vspackage.json information from a parent directory of the given source directory
+let package_json = get_package_json(options.source);
+if (package_json == null) {
   console.log('Error: vspackage.json not found in any parent directory.');
   process.exit(2);
 }
-let subfiles = getAllSubfiles(pkg.package_path);
+
+// Get all .vs files in the vspackage.json path
+let vs_files = getAllVoxelScriptSubfiles(package_json.package_path);
 
 // Create PegJs parser for .vs files
 const PEGJS_FILE = path.join(__dirname, 'voxelscript.pegjs');
 let pegjs_data = readFileSync(PEGJS_FILE, 'utf8');
 let parser = peg.generate(pegjs_data, {cache:true, trace:TRACE_PARSER});
 
+// Create compiler context
 let compiler_context = new VSCompilerContext();
 let parsing_errors : any = {};
 let voxelscripts : any = {};
 
-for(let file_data of subfiles) {
+// Loop through all vs files
+for(let file_data of vs_files) {
+  // Get data and module name
   let voxelscript_module_name = file_data.name;
   let voxelscript_data = file_data.data;
+
+  // If the script has already been accounted for, then we check for overrides
   if (voxelscripts[voxelscript_module_name]) {
     if (options.override) {
-      if (!is_subdir(options.override, file_data.filename)) {
-        // Ignoring because it's not in override
+      if (!is_subdir(options.override, file_data.filepath)) {
+        // Ignoring this version of the module, because it's not in override
         continue;
+      } else {
+        // Otherwise, we continue, in order to override it
       }
+    } else {
+      // If overrides aren't allowed, then we report an error due to duplicate modules being found
+      console.log('Error: Module ' + voxelscript_module_name + ' was found twice!');
+      console.log('First time: ' + voxelscripts[voxelscript_module_name].filepath);
+      console.log('Second time: ' + file_data.filepath);
+      process.exit(2);
     }
+
+    // If we're going to override, we need to remove the existing module
     if (voxelscripts[voxelscript_module_name].parsed) {
       compiler_context.remove_module(voxelscript_module_name);
     }
   }
+
+  // Set script filepath, code, and whether or not it's been parsed
   voxelscripts[voxelscript_module_name] = {
     code: voxelscript_data,
-    filepath: file_data.filename,
+    filepath: file_data.filepath,
     parsed: false
   };
 
+  // Initialize the parser tracer, for parser debugging
   let tracer = null;
   if (TRACE_PARSER) {
     tracer = new Tracer(voxelscript_data, {});
   }
-  // Abstract Syntax Tree
+
+  // Parse the code
   let ast = null;
   try {
     console.log("Parsing " + voxelscript_module_name + "...");
@@ -60,38 +86,45 @@ for(let file_data of subfiles) {
     if (TRACE_PARSER) {
       console.log(tracer.getBacktraceString());
     }
-    if (!err.hasOwnProperty('location')) throw(err);
-    // Slice `text` with a little context before and after the error offset
-    //console.log(err);
-    const gap = 80;
+
+    // Check if it's a valid parser error (Which always includes a location)
+    if (!err.hasOwnProperty('location')) throw err;
+
+    // Generate error string, print it, and save it under parsing errors
     let err_str = error_to_string(
-      voxelscript_module_name, file_data.filename,
+      voxelscript_module_name, file_data.filepath,
       voxelscript_data, err
     );
     console.log(err_str);
     parsing_errors[voxelscript_module_name] = err_str;
   }
 
+  // If we successfully generated an abstract syntax tree, then we add the parsed module to the compiler context
   if (ast) {
-    //print_ast(ast);
     compiler_context.add_module(voxelscript_module_name, ast);
-    console.log("Added Module: " + voxelscript_module_name + " (" + file_data.filename + ")");
+    console.log("Added Module: " + voxelscript_module_name + " (" + file_data.filepath + ")");
     console.log();
     voxelscripts[voxelscript_module_name].parsed = true;
   }
 }
 
-if (options.desired_module && !voxelscripts[options.desired_module].parsed) {
-  console.log("Could not parse desired module " + options.desired_module + "!");
-  console.log(parsing_errors[options.desired_module]);
-  process.exit(1);
+// If there is a desired module, check that it was found and parsed correctly
+if (options.desired_module) {
+  if (!(options.desired_module in voxelscripts)) {
+    console.log("Error: Desired module " + options.desired_module + " not found!");
+    process.exit(2);
+  }
+  if (!voxelscripts[options.desired_module].parsed) {
+    console.log("Could not parse desired module " + options.desired_module + "!");
+    console.log(parsing_errors[options.desired_module]);
+    process.exit(1);
+  }
 }
-
-const BASE = readFileSync(path.join(__dirname, 'base_ts.ts'));
 
 console.log("Compiling...");
 
 if (options.desired_module) {
+  // If there is a desired module, then only compile that one
   if (!compiler_context.compile_single_module(options.desired_module)) {
     console.log("Failed to compile!");
     if (compiler_context.missing_dependency) {
@@ -100,25 +133,40 @@ if (options.desired_module) {
         message: compiler_context.error_reason
       });
       console.log(err_string);
+      process.exit(1);
+    } else {
+      console.log("Unknown compiler error!");
+      process.exit(2);
     }
-    process.exit(1);
   }
 } else {
+  // Otherwise, compile all of them
   if (!compiler_context.compile_all_modules()) {
     console.log("Failed to compile!");
-    process.exit(1);
+    process.exit(2);
   }
 }
 
-if (!existsSync(options.build_target)){
-  mkdirSync(options.build_target);
-}
-writeFileSync(path.join(options.build_target, "Base.ts"), BASE);
-for (let module_name of compiler_context.get_modules()) {
-  let compiled_data = compiler_context.get_compiled_module(module_name)!;
-  if (compiled_data) {
-    writeFileSync(path.join(options.build_target, "_VS_" + module_name + ".ts"), compiled_data);
+// If there's a built target, we should get all compiled modules and write them to the build target
+if (options.build_target) {
+  // If the build target doesn't exist, make it
+  if (!existsSync(options.build_target)){
+    mkdirSync(options.build_target);
+  }
+
+  // Get base typescript file, for the resulting typescript transpilation to use
+  let base_ts_file = readFileSync(path.join(__dirname, 'base_ts.ts'));
+  writeFileSync(path.join(options.build_target, "Base.ts"), base_ts_file);
+
+  // Write the remaining compiled files to the build target directory
+  for (let module_name of compiler_context.get_modules()) {
+    let compiled_data = compiler_context.get_compiled_module(module_name)!;
+    if (compiled_data) {
+      writeFileSync(path.join(options.build_target, "_VS_" + module_name + ".ts"), compiled_data);
+    }
   }
 }
+
+// Print success message!
 console.log();
 console.log("Compilation Succeeded!");
