6905709caed7fb7d66a1a5281672d20586bf7329
Nichoals Pipitone <npip99@gmail.com>
2020-08-23 22:59:29 -0400

    setup.sh

---
+++ voxel_engine/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+sudo apt install cmake make g++ libx11-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxrandr-dev libxext-dev libxcursor-dev libxinerama-dev libxi-dev libglm-dev
+cmake .
+make
+
