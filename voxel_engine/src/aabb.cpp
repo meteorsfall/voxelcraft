b76485b3e8a736b1f6cbc8e86ef057467762afa9
Nicholas Pipitone <npip99@gmail.com>
2020-09-06 17:42:30 -0400

    add comments

--- voxel_engine/src/aabb.cpp
+++ voxel_engine/src/aabb.cpp
@@ -16,7 +16,7 @@ bool AABB::test_point(vec3 p) {
     }
 }
 
-optional<double> AABB::test_plane(vec3 point, vec3 normal) {
+bool AABB::test_plane(vec3 point, vec3 normal) {
     // Convert AABB to center-extents representation
     vec3 center = (this->max_point + this->min_point) * 0.5f; // Compute AABB center
     vec3 extents = this->max_point - center; // Compute positive extents
@@ -76,14 +76,17 @@ bool AABB::test_frustum(const mat4& PV) {
 }
 
 bool AABB::is_colliding(const AABB& other) {
-    bool max_valid = other.min_point.x < this->max_point.x
-                  && other.min_point.y < this->max_point.y
-                  && other.min_point.z < this->max_point.z;
-    bool min_valid = this->min_point.x < other.max_point.x
-                  && this->min_point.y < other.max_point.y
-                  && this->min_point.z < other.max_point.z;
+    // If our max_point is undernear the other's min_point, then we're obviously not going to collide
+    bool max_invalid = this->max_point.x <= other.min_point.x
+                    || this->max_point.y <= other.min_point.y
+                    || this->max_point.z <= other.min_point.z;
+    // If our min_point is above their max_point, then we're obviously not going to collide
+    bool min_invalid = other.max_point.x <= this->min_point.x
+                    || other.max_point.y <= this->min_point.y
+                    || other.max_point.z <= this->min_point.z;
 
-    return min_valid && max_valid;
+    // Otherwise, we must be colliding
+    return !(min_invalid || max_invalid);
 }
 
 void AABB::translate(vec3 change) {
@@ -98,6 +101,19 @@ optional<vec3> AABB::collide(AABB other) {
 
     vec3 dimensions = this->max_point - this->min_point;
 
+    //    
+    //         V-- right_needed_change = this.max.x - other.min.x
+    //         V 
+    // |--------|
+    // | this |-|-------|
+    // |--------| other |
+    //        |---------|
+    //
+    // In other words, right_needed_change is how far right the other AABB
+    // needs to move, in order to no longer be colliding with our AABB
+    // 
+
+    // We found out how much need to move in each direction
     float left_needed_change = -min(this->min_point.x - other.max_point.x, 0.0f);
     float right_needed_change =  max(this->max_point.x - other.min_point.x, 0.0f);
     float top_needed_change = -min(this->min_point.y - other.max_point.y, 0.0f);
@@ -109,16 +125,27 @@ optional<vec3> AABB::collide(AABB other) {
     float best_move_distance = 0.0;
     vec3 possible_move(0.0);
 
+    // Find the shortest distance to shove the other AABB out of this AABB
     auto lam = [&found_move, &best_move_distance, &possible_move, dimensions](float move_distance, vec3 move_dir) -> void {
+        // If right_needed_change > 0, and its less than the width,
+        // Then it's a valid move to shove the other AABB to get it out of us
+        // abs(dot(dimensions, move_dir)) tells us how wide this AABB is in that direction
         if (0 < move_distance && move_distance < abs(dot(dimensions, move_dir))) {
+            // If we haven't found a move, this is our new move
+            // If we have, but this move is a shorter distance,
+            // then it's the better move
             if (!found_move || move_distance < best_move_distance) {
                 found_move = true;
+                // Keep track of the best move distance
                 best_move_distance = move_distance;
+                // The move is how far we have to move, times the direction we need to move in
                 possible_move = move_distance*move_dir;
             }
         }
     };
 
+    // We check by trying to move the other AABB in each direction,
+    // by the amount it needs to move in that direction to exit the current AABB
     lam(left_needed_change, vec3(-1.0, 0.0, 0.0));
     lam(right_needed_change, vec3(1.0, 0.0, 0.0));
     lam(top_needed_change, vec3(0.0, -1.0, 0.0));
@@ -126,6 +153,7 @@ optional<vec3> AABB::collide(AABB other) {
     lam(forward_needed_change, vec3(0.0, 0.0, -1.0));
     lam(backward_needed_change, vec3(0.0, 0.0, 1.0));
 
+    // If we found a move, we return how we need to move to push the other AABB out of this AABB
     if (found_move) {
         return { possible_move };
     } else {
--- voxel_engine/src/aabb.hpp
+++ voxel_engine/src/aabb.hpp
@@ -3,24 +3,38 @@
 
 #include "utils.hpp"
 
+// *********
+// AABB Represents an axis-aligned bounding box
+// AABB's can 
+// *********
+
 class AABB {
 public:
     vec3 min_point;
     vec3 max_point;
 
+    // Will create an AABB with the given bottom-left point and top-right point
     AABB(vec3 min_point, vec3 max_point);
 
+    // Return true if the point p is inside the AABB
     bool test_point(vec3 p);
+    // Returns true if this AABB is colliding with another AABB
     bool is_colliding(const AABB& other);
+    // Will translate an AABB
     void translate(vec3 change);
 
-    optional<double> test_plane(vec3 point, vec3 normal);
+    // Returns true if the given plane intersects the AABB
+    bool test_plane(vec3 point, vec3 normal);
 
-    // Check if frustum intersects the AABB
-    // Note: This test may have rare false positives, claiming an interesection when there is none.
-    //       If this test returns false, then it is guaranteed that the AABB is not in the given frustum
+    // Check if frustum may be intersecting the AABB (For frustum culling)
+    // Note: This test is guaranteed to return true if the AABB actually intersects the frustum
+    // However, it might also return true for other cases as well.
+    // It will try its best to return false if the AABB is clearly not in the frustum
     bool test_frustum(const mat4& PV);
 
+    // Check if the AABB collides with another AABB, and if it does,
+    // It will return a vec3 for the direction that the other AABB must go
+    // In order to avoid collision
     optional<vec3> collide(AABB other);
 };
 
