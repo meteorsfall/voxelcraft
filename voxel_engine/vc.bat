09eaf7fbe6a3bf5ecc700bb17c5a530e73f7b7bb
Nicholas Pipitone <npip99@gmail.com>
2020-09-15 19:45:43 -0400

    fix running wavm

--- voxel_engine/vc.bat
+++ voxel_engine/vc.bat
@@ -1,2 +1,6 @@
 nmake release || EXIT /B
+if exist mods\compile_main.trigger (
+    wsl asc mods/main.ts --runtime half --explicitStart -b mods/main.wasm
+    del mods\compile_main.trigger
+)
 start /WAIT /B build/release/vc.exe
--- voxel_engine/vcd.bat
+++ voxel_engine/vcd.bat
@@ -1,2 +1,6 @@
 nmake debug || EXIT /B
+if exist mods\compile_main.trigger (
+    wsl asc mods/main.ts --runtime half --explicitStart -b mods/main.wasm
+    del mods\compile_main.trigger
+)
 start /WAIT /B build/debug/vc.exe
