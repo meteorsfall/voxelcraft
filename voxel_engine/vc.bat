156d56687fe883061208c5f3f0eea7409a79c46b
Louise Song <louise.s.song@gmail.com>
2021-03-11 01:11:54 -0500

    Only cmake during initial creation of directory

--- voxel_engine/vc.bat
+++ voxel_engine/vc.bat
@@ -1,5 +1,7 @@
-mkdir build\CMakeFiles\release
-cmake -DCMAKE_BUILD_TYPE=Release -S . -B build\CMakeFiles\release -G "Ninja"
+if not exist build\CMakeFiles\release\ (
+  mkdir build\CMakeFiles\release
+  cmake -DCMAKE_BUILD_TYPE=Release -S . -B build\CMakeFiles\release -G "Ninja"
+)
 cmake --build build\CMakeFiles\release --target voxelcraft
 cd build\release
 start /WAIT /B vc.exe
